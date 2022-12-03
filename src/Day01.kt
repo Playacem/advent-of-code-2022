9521c0d17d8a14fd64b6a1bc521956b96f2ca21c
Playacem <playacem@gmail.com>
2022-12-03 12:42:11 +0100

    Move older solutions into a more local scope

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,32 +1,32 @@
 
-class Container {
-
-    private val data: MutableList<Long> = mutableListOf()
-    private var counter: Long = 0L
-
-    fun load(input: List<String>): Container {
-        input.forEach { addLine(it) }
-        return this
-    }
-    fun addLine(line: String) {
-        if (line.isBlank()) {
-            data.add(counter)
-            counter = 0
-            return
-        }
-        counter += line.trim().toLong()
-    }
-
-    fun topValue(): Long {
-        return data.toList().max()
-    }
-
-    fun topThreeTotal(): Long {
-        return data.toList().sortedDescending().take(3).sum()
-    }
-}
-
 fun main() {
+    class Container {
+
+        private val data: MutableList<Long> = mutableListOf()
+        private var counter: Long = 0L
+
+        fun load(input: List<String>): Container {
+            input.forEach { addLine(it) }
+            return this
+        }
+        fun addLine(line: String) {
+            if (line.isBlank()) {
+                data.add(counter)
+                counter = 0
+                return
+            }
+            counter += line.trim().toLong()
+        }
+
+        fun topValue(): Long {
+            return data.toList().max()
+        }
+
+        fun topThreeTotal(): Long {
+            return data.toList().sortedDescending().take(3).sum()
+        }
+    }
+
     fun part1(input: List<String>): Long {
         return Container().load(input).topValue()
     }
@@ -38,6 +38,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000L)
+    check(part2(testInput) == 45000L)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Day02.kt
+++ src/Day02.kt
@@ -1,5 +1,3 @@
-
-
 enum class RPS(val opponentLabel: String, val ourLabel: String, val selectionValue: Int) {
     ROCK("A", "X", 1),
     PAPER("B", "Y", 2),
@@ -10,57 +8,55 @@ enum class RPS(val opponentLabel: String, val ourLabel: String, val selectionVal
     }
 }
 
-val rpsValues = RPS.values().toList()
-
-fun parseDecision(s: String): RPS {
-    return RPS.values().find { it.match(s) } ?: error("Invalid string: '$s'")
-}
-
-fun String.asRPS() = parseDecision(this)
-fun Char.asRPS() = parseDecision(this.toString())
-
-fun calculateMatchValue(match: Match): Int {
-    val (opponent, self) = match
-    if (opponent == self) {
-        return 3
-    }
-    val posDifference = self.ordinal - opponent.ordinal
-    if (posDifference in listOf(1, -2)) {
-        return 6
-    }
-    return 0
-}
-
-data class Match(val opponent: RPS, val self: RPS)
-
-fun String.asMatch(): Match {
-    val (o, s) = this.split(" ", limit = 2).map { parseDecision(it) }
-    return Match(o, s)
-}
-
-fun Match.asTotalValue(): Int {
-    return calculateMatchValue(this) + self.selectionValue
-}
-
-fun String.outcomeAsMatchString(): String {
-    val outcome = this.last()
-    val o = this.first()
-    return when(outcome) {
-        'X' -> {
-            val losingRps = rpsValues[(o.asRPS().ordinal - 1 + rpsValues.size) % rpsValues.size]
-            "$o ${losingRps.ourLabel}"
-        }
-        'Y' -> "$o $o"
-        'Z' -> {
-            val winningRps = rpsValues[(o.asRPS().ordinal + 1 + rpsValues.size) % rpsValues.size]
-            "$o ${winningRps.ourLabel}"
-        }
-        else -> error("Invalid outcome: '$outcome'")
-    }
-}
-
-
 fun main() {
+    data class Match(val opponent: RPS, val self: RPS)
+
+    val rpsValues = RPS.values().toList()
+
+    fun parseDecision(s: String): RPS {
+        return RPS.values().find { it.match(s) } ?: error("Invalid string: '$s'")
+    }
+
+    fun String.asRPS() = parseDecision(this)
+    fun Char.asRPS() = parseDecision(this.toString())
+
+    fun calculateMatchValue(match: Match): Int {
+        val (opponent, self) = match
+        if (opponent == self) {
+            return 3
+        }
+        val posDifference = self.ordinal - opponent.ordinal
+        if (posDifference in listOf(1, -2)) {
+            return 6
+        }
+        return 0
+    }
+
+    fun String.asMatch(): Match {
+        val (o, s) = this.split(" ", limit = 2).map { parseDecision(it) }
+        return Match(o, s)
+    }
+
+    fun Match.asTotalValue(): Int {
+        return calculateMatchValue(this) + self.selectionValue
+    }
+
+    fun String.outcomeAsMatchString(): String {
+        val outcome = this.last()
+        val o = this.first()
+        return when(outcome) {
+            'X' -> {
+                val losingRps = rpsValues[(o.asRPS().ordinal - 1 + rpsValues.size) % rpsValues.size]
+                "$o ${losingRps.ourLabel}"
+            }
+            'Y' -> "$o $o"
+            'Z' -> {
+                val winningRps = rpsValues[(o.asRPS().ordinal + 1 + rpsValues.size) % rpsValues.size]
+                "$o ${winningRps.ourLabel}"
+            }
+            else -> error("Invalid outcome: '$outcome'")
+        }
+    }
     fun part1(input: List<String>): Int {
         return input.map { it.asMatch() }.sumOf { it.asTotalValue() }
     }
