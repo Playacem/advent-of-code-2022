6b008efa64edcbd7e7070ea4279de71da3b85542
Playacem <playacem@gmail.com>
2022-12-03 12:38:50 +0100

    Add solution for Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,16 +1,34 @@
 fun main() {
+    fun CharRange.asLongString(): String = this.toList().joinToString(separator = "", prefix = "", postfix = "")
+
+    val alphabet: String = ('a'..'z').asLongString() + ('A'..'Z').asLongString()
+
+    fun splitInParts(string: String): List<String> {
+        return listOf(string.substring(0, string.length / 2), string.substring(string.length / 2))
+    }
+
+    fun findCommonCharacter(list: List<String>): Char {
+        val commonChars = list.map { it.toCharArray().toSet() }.reduce { acc, chars -> acc.intersect(chars) }
+        check(commonChars.size == 1) { "Unexpected size: ${commonChars.size} - $commonChars" }
+        return commonChars.first()
+    }
+
+    fun getPriorityOfItem(item: Char): Int {
+        return alphabet.indexOf(item) + 1
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { splitInParts(it) }.map { findCommonCharacter(it) }.sumOf { getPriorityOfItem(it) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.chunked(3).map { findCommonCharacter(it) }.sumOf { getPriorityOfItem(it) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 15)
-    //check(part2(testInput) == 12)
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
